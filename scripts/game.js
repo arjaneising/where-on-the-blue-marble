99df2b853266464612308524797cd4795af1cdff
Arjan Eising <arjan@arjaneising.nl>
2013-04-20 21:20:40 +0200

    Added gameplay controls

--- scripts/game.js
+++ scripts/game.js
@@ -1,20 +1,27 @@
-(function($, Robinson) {
-  var pickGameElm, playGameElm, gameType, rbnsn;
-
+(function($, Robinson, Modernizr) {
+  var pickGameElm, playGameElm, gameType, overlayElm, rbnsn, canTap, startedTime, currentInfo, canvasElm, ctx, totalPoints;
 
 
 
 
   var init = function() {
+    canTap = false;
+
     pickGameElm = $('.pick-game').removeClass('hide');
     playGameElm = $('.play-game');
+    overlayElm = $('.overlay');
+    resultElm = $('.result');
 
     // start a game
     pickGameElm.on('change', 'input', startGame);
+    resultElm.on('click', '.next', nextImage);
 
     // init the map details
     rbnsn = new Robinson(333, 650, -13);
     setMap();
+
+
+    setInterval(checkTimer, 400);
   };
 
 
@@ -27,10 +34,27 @@
 
     gameType = value;
 
+    totalPoints = 0;
+
     pickGameElm.addClass('hide');
     playGameElm.removeClass('hide');
 
+    startTimer();
+
     requestImagery(showImage, requestError);
+  };
+
+
+  var nextImage = function(evt) {
+    evt.preventDefault();
+    requestImagery(showImage, requestError);
+    resultElm.addClass('hide');
+    overlayElm.find('img').addClass('hide');
+  };
+
+
+  var stopGame = function() {
+    console.log('WOEI');
   };
 
 
@@ -43,27 +67,34 @@
     });
 
     promise.then(cb, err);
+
+    canTap = true;
   };
 
 
   // set map events
   var setMap = function() {
-    var map;
+    overlayElm.hammer().on('tap', handleTap);
+  };
 
-    map = $('.map');
 
-    map.hammer().on('tap', function(evt) {
-      var offset, srcEvent;
 
-      offset = $(evt.currentTarget).offset();
-      srcEvent = evt.gesture.srcEvent;
+  var handleTap = function(evt) {
+    if (!canTap) {
+      return;
+    }
 
-      latlng = rbnsn.xy2latlng(srcEvent.clientX - offset.left, srcEvent.clientY - offset.top);
+    console.log('woei');
+    
+    var offset, srcEvent;
 
-      console.log('LAT', latlng.lat);
-      console.log('LNG', latlng.lng);
-    });
-  }
+    offset = $(evt.currentTarget).offset();
+    srcEvent = evt.gesture.srcEvent;
+
+    latlng = rbnsn.xy2latlng(srcEvent.clientX - offset.left, srcEvent.clientY - offset.top);
+
+    showAnswer(latlng);
+  };
 
 
 
@@ -71,6 +102,36 @@
     var url;
     url = data.url;
     playGameElm.find('.photo img').attr('src', url);
+    currentInfo = data;
+  };
+
+
+
+
+
+
+  var showAnswer = function(latlng) {
+    var lat, lng, xy, dist;
+
+    lat = parseFloat(currentInfo.lat, 10);
+    lng = parseFloat(currentInfo.lon, 10);
+
+    xyClicked = rbnsn.latlng2xy(latlng.lat, latlng.lng);
+    xyReal = rbnsn.latlng2xy(lat, lng);
+
+    overlayElm.find('img').andSelf().removeClass('hide');
+    overlayElm.find('.flag-green').css({left: xyReal.x + 'px', top: xyReal.y + 'px'});
+    overlayElm.find('.flag-gray').css({left: xyClicked.x + 'px', top: xyClicked.y + 'px'});
+    
+    dist = distance(latlng.lat, latlng.lng, lat, lng);
+    points = 67;
+
+    totalPoints += points;
+
+    $('.score').text('Score: ' + totalPoints + ' points');
+
+    resultElm.removeClass('hide');
+    resultElm.find('.points').text("That's a distance of " + ~~dist + ' kilometers. You scored ' + points + ' points with that!');
   };
 
 
@@ -79,8 +140,56 @@
     if (console) {
       console.log('Error');
     }
+  };
+
+
+  var startTimer = function() {
+    startedTime = new Date;
+  };
+
+
+  var checkTimer = function() {
+    if (!startedTime) {
+      return;
+    }
+
+    var now, minutes, seconds;
+
+    now = new Date;
+    humanTime = 120 - ~~((now.getTime() - startedTime.getTime()) / 1000);
+
+    if (humanTime < 0) {
+      stopGame();
+    }
+
+    minutes = ~~(humanTime / 60);
+    seconds = humanTime % 60;
+
+    seconds = seconds.toFixed(0);
+
+    if (seconds.length  === 1) {
+      seconds = '0' + seconds;
+    }
+
+    $('.timer').text('Time left: ' + minutes + ':' + seconds);
+  };
+
+
+
+  var distance = function(lat1, lng1, lat2, lng2) {
+    var R, dLat, dLng, a, c;
+
+    R = 6371;
+    dLat = (lat2 - lat1) / (Math.PI / 180);
+    dLng = (lng2 - lng1) / (Math.PI / 180);
+    lat1 = lat1 / (Math.PI / 180);
+    lat2 = lat2 / (Math.PI / 180);
+
+    a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.sin(dLng / 2) * Math.sin(dLng / 2) * Math.cos(lat1) * Math.cos(lat2); 
+    c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a)); 
+    return R * c; 
   }
 
 
   init();
-})(jQuery, Robinson);
+})(jQuery, Robinson, Modernizr);
